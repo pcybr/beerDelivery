2d4c01b839445106550f383770593ae542086d1e
pcybr <pic5fe@virginia.edu>
2018-02-14 00:07:22 -0500

    Admin Changes

--- beerDelivery/myapp/admin.py
+++ beerDelivery/myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Beer, Store, Order, Person, Trip
 
+
+admin.site.register(Beer)
+admin.site.register(Person)
+admin.site.register(Store)
+admin.site.register(Trip)
+admin.site.register(Order)
 # Register your models here.
