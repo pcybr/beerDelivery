759fb225b4f998a50ac174204a993fb43f036ff0
wsf11 <winstonfrick@gmail.com>
2018-02-14 13:22:20 -0500

    Created JSON responses for two models

--- beerDelivery/myapp/urls.py
+++ beerDelivery/myapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^beerdelivery/', views.index, name = 'index'),
+    url(r'^api/v1/person/(?P<pk>[-\w]+)/$', views.ApiPersonGetView, name='person_get'),
+    url(r'^api/v1/beer/(?P<pk>[-\w]+)/$', views.ApiBeerGetView, name='beer_get'),
+    
+
 ]
