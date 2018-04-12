b97b3e3145d329e8438af7f9b5ac39494024fbc4
ntuz <nat5ds@virginia.edu>
2018-04-12 16:57:51 -0400

    trip listing kafka

--- batch/script.py
+++ batch/script.py
@@ -1,23 +1,25 @@
 from elasticsearch import Elasticsearch
-from kafka import KafkaProducer
+from kafka import KafkaConsumer
+import json
 
 while True:
 	try:
-		consumer = KafkaConsumer('trip_topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
-		consumer2 = KafkaConsumer('order_topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+		consumer = KafkaConsumer('trip_topic', group_id='listing-index', bootstrap_servers=['kafka:9092'])
+		# consumer2 = KafkaConsumer('order_topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
 		es = Elasticsearch(['es'])
 
 		for index in consumer:
 			listing = json.loads((index.value).decode('utf-8'))
 			print("Trip Listing",listing)
-			es.index(index='listing_index', doc_type='listing', id=listing['trip_id'], body=listing[0])
+			es.index(index='listing_index', doc_type='listing', id=listing['trip_id'], body=listing)
 			es.indices.refresh(index="listing_index")
 
-		for index in consumer2:
-			listing = json.loads((index.value).decode('utf-8'))
-			print("Trip Listing",listing)
-			es.index(index='listing_index', doc_type='listing', id=listing['order_id'], body=listing[0])
-			es.indices.refresh(index="listing_index")
+		# for index in consumer2:
+		# 	print('loop 2')
+		# 	listing = json.loads((index.value).decode('utf-8'))
+		# 	print("Order Listing",listing)
+		# 	es.index(index='listing_index', doc_type='listing', id=listing['order_id'], body=listing[0])
+		# 	es.indices.refresh(index="listing_index")
 
 		continue
 
