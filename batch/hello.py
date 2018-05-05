d5d409afa2e4b4c47fe51d08380d562e035a2586
wsf11 <winstonfrick@gmail.com>
2018-05-05 15:14:28 -0400

    Spark stuff

--- batch/hello.py
+++ batch/hello.py
@@ -1,4 +1,6 @@
 from pyspark import SparkContext
+import MySQLdb
+import _mysql
 
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
 
@@ -43,4 +45,28 @@ filtered = counts.filter(lambda pair: pair[1] > 2)
 #Collect all output data
 output = filtered.collect()
 
+db = MySQLdb.connect(host="db", user="www", passwd="$3cureUS", db="cs4501")
+
+cursor = db.cursor()
+
+cursor.execute("""DELETE FROM myapp_recommendation""")
+
+dic = {}
+for key,value in output:
+	item_id = int(key[0])
+	recommended_trips = int(key[1])
+	if item_id in dic:
+		dic[item_id]+="," + str(recommended_trips)
+	else:
+		dic[item_id] = str(recommended_trips)
+
+for key, value in dic:
+	cursor.execute("""INSERT INTO myapp_recommendation (item_id,recommended_trips) VALUES (%d,%s)""",(int(key),str(value)))
+
+cursor.execute("""SELECT * FROM myapp_recommendation""")
+print(cursor.fetchall())
+
+cursor.close()
+db.commit()
+db.close()
 sc.stop()
